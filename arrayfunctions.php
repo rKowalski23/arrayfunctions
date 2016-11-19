9334f668bb65ed2776e4cd411d9024047152fb02
Raymond F Kowalski <rfk4@afsconnect1.njit.edu>
2016-11-19 17:01:26 -0500

    changed echos to writelns for easier line breaks

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -1,44 +1,47 @@
 <?php
 //1
 $array1 = array('a', 'b', 'c', 'd', 'e');
-echo array_chunk($array1, 2);
+writeln(array_chunk($array1, 2));
 //2
 $array_keys = array('0', '1', '2', '3', '4');
-echo array_combine($array_keys, $array1);
+writeln(array_combine($array_keys, $array1));
 //3
-echo array_count_values($array1);
+writeln(array_count_values($array1));
 //4
-echo array_diff($array1, $array_keys);
+writeln(array_diff($array1, $array_keys));
 //5
 $fill_this_array = array_fill(0, 5, 'value');
-echo $fill_this_array;
+writeln($fill_this_array);
 //6
 $fill_this_array2 = array_fill_keys($array_keys, 'value');
-echo $fill_this_array2;
+writeln($fill_this_array2);
 //7
 $fill_this_array3 = array_flip($fill_this_array2);
-echo $fill_this_array3;
+writeln($fill_this_array3);
 //8
-echo array_key_exists('a', $array1);
+writeln(array_key_exists('a', $array1));
 //9
-echo array_keys($fill_this_array2);
+writeln(array_keys($fill_this_array2));
 //10
 $array3 = array_merge($array1, $array2);
-echo $array3;
+writeln($array3);
 //11
 $array4 = array('zero', 'one', 'two', 'three');
-echo array_pad($array4, 8, 'padding');
+writeln(array_pad($array4, 8, 'padding'));
 //12
-echo array_pop($array4);
+writeln(array_pop($array4));
 //13
 $random_value = array_rand($array4);
-echo $random_value;
+writeln($random_value);
 //14
-echo array_reverse($array4);
+writeln(array_reverse($array4));
 //15
 $array_nums = array(1, 2, 4, 5, 7, 8);
-echo array_sum($array_nums);
+writeln(array_sum($array_nums)); 
 
-
+function writeln($line_in) 
+{
+  echo $line_in.'<br/>';
+}
 
 ?>
